4af43d88a844393c6aa5d08c50ba772302746e75
Benjamin Ojanne <benjaminprogrammering@gmail.com>
2020-11-23 16:21:33 +0100

    commit.4

--- filter.js
+++ filter.js
@@ -1,6 +1,12 @@
 
 function filter(arr, func) {
-
+    const newArray = []
+    for (let i = 0; i < arr.length; i++) {
+        if (func(arr[i])) {
+            newArray.push(arr[i])
+        }
+    }
+    return newArray
 }
 
 // TEST 1
@@ -9,6 +15,12 @@ const filteredNumbers = filter(numbers, x => x < 10)
 console.log(filteredNumbers)
 
 // TEST 2 - skriv själv
+const numbers2 = [5, 1, 2, 100, 25, 24, 4, 1, 0, -1, 5, 50]
+const filteredNumbers2 = filter(numbers2, x => x < 10)
+console.log(filteredNumbers2)
 
 
 // TEST 3 - skriv själv
+const numbers3 = [55, 2, 3, 4, 5]
+const filteredNumbers3 = filter(numbers3, x => x < 10)
+console.log(filteredNumbers3)
