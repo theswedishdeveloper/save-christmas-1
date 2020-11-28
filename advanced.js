c8f2929c36e08b219c9ba486bdf712319e2f2cf9
Benjamin <=>
2020-11-28 17:48:34 +0100

    small update

--- advanced.js
+++ advanced.js
@@ -1,5 +1,5 @@
 const isPrimeFunc = (num) => {
-    for (var i = 2; i < num; i++)
+    for (let i = 2; i < num; i++)
         if (num % i === 0) return false
     return num > 1
 }
--- statistics.js
+++ statistics.js
@@ -1,13 +1,13 @@
 
 function max(arr) {
-    let maxValue = 0
+    let maxValue = arr[0]
     //använde en forEach loop här ist för en for loop.
     arr.forEach((item) => { if (item > maxValue) maxValue = item })
     return maxValue
 }
 
 function min(arr) {
-    let minValue = max(arr)
+    let minValue = max(arr) //start from the biggest number.
     //använde en forEach loop här ist för en for loop.
     arr.forEach((item) => { if (item < minValue) minValue = item })
     return minValue
@@ -19,6 +19,9 @@ function mean(arr) {
     for (let i = 0; i < arr.length; i++) {
         meanResult += arr[i]
     }
-    meanResult = meanResult / arr.length
+
+    if (meanResult != 0) //prevent divide by zero :D
+        meanResult = meanResult / arr.length
+
     return meanResult
 }
